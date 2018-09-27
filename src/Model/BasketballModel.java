25c6ee33e09b2fc09b6d6df72215c7f4db26dad7
ZachFleck <zacfleck10@gmail.com>
2018-09-27 12:48:52 -0600

    I fixed some errors  and added th try catchand I added attempt percentage

--- src/Model/BasketballModel.java
+++ src/Model/BasketballModel.java
@@ -4,13 +4,13 @@ import interesante.BasketballController;
 
 public class BasketballModel
 {
-
+	private BasketballController yourPractice;
 	private String name;
 	private int timeMinutes;
 	private double attemptPercentage;
 	private int attemptAmount;
 	private boolean practice;
-
+	private int attemptsMade;
 
 	{
 		yourPractice = new BasketballController();
@@ -45,6 +45,11 @@ public class BasketballModel
 	{
 	return practice;
 	}
+	
+	public int getAttemptsMade()
+	{
+		return attemptsMade;
+	}
 		//------------------------------------setters------------------------------
 		
 		
@@ -52,13 +57,26 @@ public class BasketballModel
 		{
 			this.name = name;
 		}
-	
-	
-	
-	
-	
-	
-	
+		public void setAttemptPercentage(double attemptPercentage)
+		{
+			this.attemptPercentage = attemptPercentage;
+		}
+		public void setAttemptAmount(int attemptAmount)
+		{
+			this.attemptAmount = attemptAmount;
+		}
+		public void setTimeMinutes(int timeMinutes)
+		{
+			this.timeMinutes = timeMinutes;
+		}
+		public void setPractice(boolean practice)
+		{
+			this.practice = practice;
+		}
+		public void setAttemptsMade(int attemtsMade)
+		{
+			this.attemptsMade = attemptsMade;
+		}
 	
 	
 	
--- src/interesante/BasketballController.java
+++ src/interesante/BasketballController.java
@@ -1,4 +1,5 @@
 package interesante;
+
 import javax.swing.JOptionPane;
 import Model.BasketballModel;
 
@@ -18,33 +19,50 @@ public class BasketballController
 
 	private void loopy()
 	{
-
+		BasketballModel  myPractice = new BasketballModel();
 		boolean isfinished = false;
 		int someCount = 0;
-		
+
 		while (!isfinished)
 		{
 			JOptionPane.showMessageDialog(null, "annoy everyone");
 		}
-			
+
 		someCount++;
-someCount += 1;
+		someCount += 1;
+
+		if (someCount > 10)
+		{
+			isfinished = true;
+		}
+
+		String response = JOptionPane.showInputDialog(null, "how many shots have been made");
+		while (!validDouble(response))
+		{
+			JOptionPane.showMessageDialog(null, "got the right answer ");
+		}
+		response = JOptionPane.showInputDialog(null, "Got to do the right answer");
+		myPractice.setAttemptPercentage(Double.parseDouble(response));
+	}
 
 	
-	if (someCount > 10)
+
+	public boolean validDouble (String mightbeDouble)
 	{
-		isfinished = true;
-	}
-	
-	String response = JOptionPane.showInputDialog(null,"how many shots have been made");
-	while(!validDouble(response))
+		boolean isValid = false;
+		
+		try
+		{
+
+	Double.parseDouble(mightbeDouble);
+			isValid = true;
+		}catch(
+
+	NumberFormatException error)
 	{
-	JOptionPane.showMessageDialog(null, "got the right answer ");
-	}
-	response = JOptionPane.showInputDialog(null, "Got to do the right answer");
-	}
-	.setShotsMade(Double.parsDouble(response));
-	
-	
+					JOptionPane.showMessageDialog(null, "type in a decimal aka a number");
 		}
-}
+
+	return isValid;
+
+}}
